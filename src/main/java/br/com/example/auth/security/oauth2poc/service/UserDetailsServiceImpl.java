faee49cc119245b59609f9c16b6aab0312de5307
ramonqueiroga <ramon.araujo.queiroga@gmail.com>
2020-06-10 00:17:23 -0300

    add granted authorities

--- src/main/java/br/com/example/auth/security/oauth2poc/service/UserDetailsServiceImpl.java
+++ src/main/java/br/com/example/auth/security/oauth2poc/service/UserDetailsServiceImpl.java
@@ -3,6 +3,9 @@ package br.com.example.auth.security.oauth2poc.service;
 import br.com.example.auth.security.oauth2poc.domain.Role;
 import br.com.example.auth.security.oauth2poc.infra.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,6 +18,7 @@ import java.util.stream.Collectors;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+    private static final String ROLE = "ROLE_";
 
     @Autowired
     private UserRepository userRepository;
@@ -23,6 +27,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         br.com.example.auth.security.oauth2poc.domain.User user = this.userRepository.findByUsername(username);
         List<String> roles = user.getRoles().stream().map(Role::getName).collect(Collectors.toList());
-        return new User(user.getUsername(), "{bcrypt}$2a$10$yXmeUSpWCA.X9snxXS6C2eR8nrvIkaXqSJRINq0R1rFi7IqrgRhg.", new ArrayList<>());
+        List<SimpleGrantedAuthority> grantedAuthorities = roles.stream()
+                .map(s -> ROLE + s)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+        return new User(user.getUsername(), "{bcrypt}$2a$10$yXmeUSpWCA.X9snxXS6C2eR8nrvIkaXqSJRINq0R1rFi7IqrgRhg.", grantedAuthorities);
     }
 }
